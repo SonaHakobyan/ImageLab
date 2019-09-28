cb2002adad7274845d4b559acd5498c5cffe5a6f
Hakobyan Sona <hakobyansona.98@gmail.com>
2019-09-28 23:32:53 +0400

    Initial commit

---
+++ ImageLab/ImageLab/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ImageLab.ViewModels;
+using System.Windows;
+
+namespace ImageLab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ ImageLab/ImageLab/ViewModels/BindableBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ImageLab.ViewModels
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
